81faa4e9468b8194a17bf77a91387f78ab3d27fa
wuchongxin <100920894+wuchongxin@users.noreply.github.com>
2023-10-28 21:55:39 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -487,8 +487,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default=ROOT / 'yolov5n.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='models/yolov5n-Ghostconv-C3Ghost-BiFPN_Concat-CBAMC3.yaml', help='model.yaml path')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/ves.yaml', help='dataset.yaml path')
+    parser.add_argument('--cfg', type=str, default='models/yolov5n_LEAS.yaml', help='model.yaml path')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/dataset.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
--- val.py
+++ val.py
@@ -336,8 +336,8 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default=ROOT / 'data/ves.yaml', help='dataset.yaml path')
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model.pt path(s)')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/dataset.yaml', help='dataset.yaml path')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'model.pt', help='model.pt path(s)')
     parser.add_argument('--batch-size', type=int, default=32, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=256, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
